17e8709b0fef36b1ba9349b77dd9fd70487d4319
Quan (Quant) H. TRAN <quant@enclave.vn>
2017-10-12 16:46:52 +0700

    Change project

---
+++ FindAPlace/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FindAPlace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
